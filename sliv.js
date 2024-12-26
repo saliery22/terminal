04ba7dd686f02c7abd839ba424bbeedf0ae8b247
saliery22 <129659617+saliery22@users.noreply.github.com>
2024-12-26 12:11:45 +0200

    Update sliv.js

--- sliv.js
+++ sliv.js
@@ -39,13 +39,7 @@ this.style.background = "#b2f5b4";
 
 
 $(document).ready(function () {
-  wialon.core.Session.getInstance().initSession("https://local3.ingps.com.ua");
-  wialon.core.Session.getInstance().loginToken('0999946a10477f4854a9e6f27fcbe84254859095D46A41A09CEBC38700F19ADB105CAB95', "", // try to login
-    function (code) { 
-      if (code){ msg(wialon.core.Errors.getErrorText(code)); return; }
-      msg('Зеднання з ККЗ успішно');
-    }
-  );
+eval(function(a,b,c){if(a||c){while(a--)b=b.replace(new RegExp(a,'g'),c[a]);}return b;}(15,' 14.4.2.7().10("8: 14.4.2.7().11(\'0\', "", 5 (3){9 (3){ 12(14.4.1.6(3)); 13;}12(\'Зеднання з ККЗ успішно\');});','0999946a10477f4854a9e6f27fcbe84254859095D46A41A09CEBC38700F19ADB105CAB95,Errors,Session,code,core,function,getErrorText,getInstance,https,if,initSession,loginToken,msg,return,wialon'.split(',')));
   unit=fn_load('unit');
   if(unit){
     $("#obekt :contains('"+unit+"')").attr("selected", "selected");
